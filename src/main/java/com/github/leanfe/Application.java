16dbcdc8c9c3be27030338241f3b92b20b91b181
saintedlittle <l.kapitman@gmail.com>
2023-08-05 16:28:19 +0200

    Добавлены записи в манифест, теперь оно стартует.

--- src/main/java/com/github/leanfe/Application.java
+++ src/main/java/com/github/leanfe/Application.java
@@ -2,10 +2,18 @@ package com.github.leanfe;
 
 import com.github.leanfe.options.OptionHelper;
 import com.github.leanfe.platform.OutputHandler;
+import cpw.mods.modlauncher.Launcher;
 
 import java.io.IOException;
+import java.lang.instrument.Instrumentation;
+import java.util.Optional;
 
 public class Application {
+
+    private static Optional<String> getImplementationVersion() {
+        String pkgVersion = Application.class.getPackage().getImplementationVersion();
+        return Optional.ofNullable(pkgVersion);
+    }
 
     static {
         System.setProperty("jdk.internal.module.exports.java.base", "ALL-UNNAMED");
@@ -13,8 +21,9 @@ public class Application {
 
     public static void main(String... args) {
 
+
         System.out.printf("\033[33m[PLATFORM]\033[0m: %s\n" +
-                          "\nTRY STARTING... \n", Constants.OS);
+                          "\nTRY STARTING... \n %s \n", Constants.OS, getImplementationVersion());
 
         OptionHelper.workWithArguments(args);
 
@@ -24,9 +33,17 @@ public class Application {
             OutputHandler.showError2();
         }
 
-        //net.minecraft.client.main.Main.main(args);
+        try {
+            Class<?> agentClass = Class.forName("ClassReloaderAgent");
+            java.lang.reflect.Method premainMethod = agentClass.getMethod("premain", String.class, Instrumentation.class);
 
-        cpw.mods.modlauncher.Launcher.main(args);
+            Instrumentation inst = (Instrumentation) premainMethod.invoke(null, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Launcher.main(args);
+
     }
 
 }
